657654b796c27f4df700bccf707ae8859ceffee6
dunyung1 <dshege4@gmail.com>
2019-02-17 16:09:27 +0300

    install flask login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -7,7 +8,38 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
 
-    def __repr__(self):
+    writer_id = db.Column(db.Integer, db.ForeignKey('writer.id'))       #one writer is shared by many users
+
+    def __repr__(self):     #makes it easier to debug our applications.
         return f'User {self.username}'
 
+class Writer(db.Model):
+    __tablename__ = 'writer'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure = db.Column(db.String(255))
+
+    users = db.relationship('User',backref = 'writer',lazy="dynamic")     #db.relationship to create a virtual column that will connect with the foreign key
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.name}'
         
+
+
+
+
+
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server     #installed to allow us create command line parsers
-from app.models import User
+from app.models import User,Writer
 from flask_migrate import Migrate, MigrateCommand 
 
 # Creating app instance
@@ -9,12 +9,19 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Writer = Writer )
 
 if __name__ == '__main__':
     manager.run()
