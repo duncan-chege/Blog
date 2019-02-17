97c0e9e1ec8e0db4f342cb56169b884cb4d20786
dunyung1 <dshege4@gmail.com>
2019-02-17 18:22:04 +0300

    download bootstrap extension & create login view

---
+++ app/auth/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import Writer
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+    
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,22 @@
-from flask import render_template
-from . import auth
+from flask import render_template,redirect,url_for,flash,request
+from ..models import Writer
+from .forms import LoginForm
+from flask_login import login_user
+from .. import db
 
-@auth.route('/login')
+
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        writer = Writer.query.filter_by(email = login_form.email.data).first()
+        if writer is not None and writer.verify_password(login_form.password.data):
+            login_user(writer,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid name or password')
+
+    title='Techie Talk login'
+    return render_template('auth/login.html',login_form = login_form, title = title)
+
+   
