2988ecdd34f760a0e249fba03c90e755509f5e58
dunyung1 <dshege4@gmail.com>
2019-02-17 06:40:59 +0300

    install sql alchemy into the app

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dunyung1:iamyung1@localhost/myblog'
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
