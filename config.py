0b0577c82a38ac7797864f885d0c5b1a67982484
dunyung1 <dshege4@gmail.com>
2019-02-19 17:11:34 +0300

    final changes

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dunyung1:iamyung1@localhost/myblog'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
