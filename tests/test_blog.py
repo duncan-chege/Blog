3462cc766fec8778708532f7224db6f294188b5f
Duncan Chege <dshege4@gmail.com>
2019-02-20 11:29:47 +0300

    Update and rename test_quote.py to test_blog.py

---
+++ tests/test_blog.py
@@ -0,0 +1,15 @@
+import unittest
+from app.models import Blog
+
+class BlogTest(unittest.TestCase):
+    '''
+    Blog Class to test the behaviour of the Quote class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_blog = Quote('Apostle Paul', 'Labour in the Lord is not in vain')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_blog,Blog))
